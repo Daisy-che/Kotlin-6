faa1116f31d8139b6cb6e55196704018d60cfd44
Student <daisychepkiruih@gmail.com>
2024-03-12 20:32:41 +0300

    add file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+@file:Suppress("UNREACHABLE_CODE")
+
+fun main() {
+println(calculateAverage(6,4))
+    println(isPrime(2))
+}//
+//Write a Kotlin function named calculateAverage that takes a variable number of integers as parameters and returns their average. Print the result.
+fun calculateAverage(num:Int,num2:Int): Int {
+    var numbers= (6+4)/2
+    return  numbers
+
+}//Create a Kotlin function named isPrime that takes an integer as a parameter and returns true if it's a prime number and false otherwise. Print the result.
+fun isPrime(num:Int): Boolean {
+    var primeNumber=2
+    if (2===" ")
+        return true
+    else
+        return false
+}
